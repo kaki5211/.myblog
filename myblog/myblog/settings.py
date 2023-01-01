75fb0ec001f8af89815858cc71a534dfef7fd3bf
kaki <kaki@aa.com>
2023-01-02 00:25:07 +0900

    2023-1-2-5

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -204,7 +204,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'media')
 
 
 # Default primary key field type
